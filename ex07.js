7c10b9c28f15c6e822a6686e180913649f7a3ef1
Phil <xuyuwei19940909@gmail.com>
2024-10-01 10:49:40 -0700

    ex07 done

--- ex07.js
+++ ex07.js
@@ -3,5 +3,16 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
+function validateEmail(emailString) {
+    let necessaryPresence = ['@', '.'];
+    let standerCount = 0;
+
+    for (let presence of necessaryPresence) {
+        if (emailString.includes(presence)) standerCount++
+    }
+
+    return standerCount === necessaryPresence.length;
+}
+
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
