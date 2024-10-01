890517c24e3d18f6424b7df445bfca66055a4dbb
Phil <xuyuwei19940909@gmail.com>
2024-10-01 09:53:00 -0700

    ex04 done

--- ex04.js
+++ ex04.js
@@ -2,4 +2,25 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+function mostFrequentChar(inputString) {
+    let chars = inputString.split('');
+    let uniChars = new Set(chars);
+    let rank = []
+
+    uniChars.forEach(uniChar => {
+        let charObject = {
+            char: uniChar,
+            count: 0
+        };
+
+        chars.map((char) => {
+            if (char === uniChar) charObject.count++
+        })
+
+        rank.push(charObject)
+    })
+
+    return rank.sort((a, b) => b.count - a.count).at(0).char
+}
+
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
