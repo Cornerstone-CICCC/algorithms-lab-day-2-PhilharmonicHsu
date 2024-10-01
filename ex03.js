c7fd8ee20db8e594698648a6d112a195d3b0c027
Phil <xuyuwei19940909@gmail.com>
2024-10-01 09:15:39 -0700

    ex03 done

--- ex03.js
+++ ex03.js
@@ -2,4 +2,18 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+function findMissingNumber(inputArray) {
+    let minimum = inputArray.at(0);
+    let maximum = inputArray.at(-1);
+    let missingNumbers = [];
+
+    for (let i = minimum ; i <= maximum; i++) {
+        if (! inputArray.includes(i)) {
+            missingNumbers.push(i);
+        }
+    }
+
+    return missingNumbers;
+}
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
