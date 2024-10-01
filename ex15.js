443c47433f16d170186260169b0081ce7228bdce
Phil <xuyuwei19940909@gmail.com>
2024-10-01 11:32:54 -0700

    ex15 done

--- ex15.js
+++ ex15.js
@@ -2,4 +2,8 @@
 // and returns the maximum number in the array.
 // Example: findMax([3, 1, 4, 1, 5, 9]) should return 9.
 
+function findMax(numbers) {
+    return numbers.toSorted((a, b) => b - a).at(0);
+}
+
 console.log(findMax([3, 1, 4, 1, 5, 9])); // Expected output: 9
