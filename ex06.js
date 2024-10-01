c452771c1e00e5d5d81a3a4b67c3c45b354d50ed
Phil <xuyuwei19940909@gmail.com>
2024-10-01 10:35:21 -0700

    ex06 done

--- ex06.js
+++ ex06.js
@@ -3,4 +3,21 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+function getQueryParams(url) {
+    let queryStrings = url.split('?')[1];
+    let queryParams = {};
+
+    if (queryStrings === undefined) {
+        return queryParams;
+    }
+
+    queryStrings.split('&').map((queryString) => {
+        let query = queryString.split('=')
+
+        queryParams[query[0]] = query[1];
+    })
+
+    return queryParams;
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
